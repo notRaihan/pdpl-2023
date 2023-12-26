16ba6efadc0b2c35e28df83db9a955e03a2ffac0
notRaihan <114415754+notRaihan@users.noreply.github.com>
2023-12-26 15:41:58 +0700

    refactor: response, singleton pattern

--- models/todolistModel.java
+++ models/todolistModel.java
@@ -18,8 +18,36 @@ interface todolistInterface {
  * todolistModel
  */
 public class todolistModel implements todolistInterface {
+
+    /*
+     * singleton pattern
+     */
+
+     // create private static instance of todolistModel, so there's only one instance of todolistModel
+    private static todolistModel instance = new todolistModel();
+
+    // make constructor private, so it can't be accessed from outside
+    private todolistModel() {}
+
+    // get the only instance of todolistModel
+    public static todolistModel getInstance() {
+        return instance;
+    }
     
+    /*
+     * end of singleton pattern
+     */
+
+
+
+
+
+    // create hashmap to store tasks
     public HashMap<Integer, taskModel> tasks = new HashMap<Integer, taskModel>();
+
+
+
+
 
     // create task and add to tasks hashmap, return success message (create)
     public String[] create(String task) {
@@ -31,15 +59,18 @@ public class todolistModel implements todolistInterface {
         tasks.put(tasks.size() + 1, newTask);
 
         // return success message
-        String[] result = new String[2];
-        result[0] = "success";
-        result[1] = "Tugas berhasil ditambahkan";
+        String[] result = {"success", "Task berhasil ditambahkan"};
         return result;
     }
 
 
     // read all tasks from tasks hashmap, print all tasks (read)
     public void getAllTasks() {
+
+        if (tasks.size() == 0) {
+            System.out.println("yay, Tidak ada tugas!");
+            return;
+        }
         for (int i = 1; i <= tasks.size(); i++) {
             System.out.println("Task ID " + i + ": " + tasks.get(i).getTask() + " - " + tasks.get(i).getStatus());
         }
