9c39784f9095743083b6c62afb436fbec130715d
HannaAnggraini06 <114386055+HannaAnggraini06@users.noreply.github.com>
2023-12-27 14:37:41 +0700

    Update prompts.java

--- controllers/prompts.java
+++ controllers/prompts.java
@@ -46,12 +46,32 @@ public class prompts {
 
                         menu.back(scanner);
                         break;
-                    case 3:
-                        // clear screen with escape sequence
+                   case 3:
+                         // clear screen with escape sequence
                         System.out.print("\033[H\033[2J");
 
-                        System.out.println("Ubah todolist");
+                        taskModel.getAllTasks();
+                        System.out.print("Pilih nomor todolist yang ingin diubah: ");
+                        int taskIndexToUpdate = scanner.nextInt();
+                        scanner.nextLine(); // consume newline
+
+                        // Check if the index is valid
+                        if (taskIndexToUpdate >= 1 && taskIndexToUpdate <= taskModel.getTaskCount()) {
+                        System.out.print("Masukkan perubahan untuk todolist: ");
+                        String updatedTaskName = scanner.nextLine();
+
+                        // Update the task
+                        for (String task : taskModel.update(taskIndexToUpdate, updatedTaskName)) {
+                        System.out.println(task);
+                        }
+                        System.out.println("Todolist berhasil diubah.");
+                        } else {
+                        System.out.println("Nomor todolist tidak valid.");
+                        }
+
+                        menu.back(scanner);
                         break;
+
                     case 4:
                         // clear screen with escape sequence
                         System.out.print("\033[H\033[2J");
